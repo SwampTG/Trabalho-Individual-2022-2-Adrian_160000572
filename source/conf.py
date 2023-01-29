e0ce0ccc909085d38ba8a9a5b3cb9e22467712fb
SwampTG <adriansoareslps@gmail.com>
2023-01-28 21:44:11 -0300

    Sphinx auto doc configured

---
+++ source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("./../src/"))
+
+project = "trabalho-individual-2022-2-adrian-160000572"
+copyright = "2023, Lappis - Unb/FGA"
+author = "Lappis - Unb/FGA"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+master_doc = "index"
---
+++ source/modules.rst
@@ -0,0 +1,38 @@
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Key smash
+=================
+.. automodule:: data_pipeline.feature_engineering.key_smash
+   :members:
+
+Word embedding
+=================
+.. automodule:: data_pipeline.feature_engineering.word_embedding
+   :members:
+
+Concatenate columns
+================
+.. automodule:: data_pipeline.pre_processing.concatenate_columns
+   :members:
+
+Feature Parser
+=================
+.. automodule:: parser.feature_engineering_parser
+   :members:
+
+Model Parser
+=================
+.. automodule:: parser.model_parser
+   :members:
+
+Base Parser
+=================
+.. automodule:: parser.parser_base
+   :members:
+
+YAML Parser
+=================
+.. automodule:: parser.YAML_parser
+   :members:
